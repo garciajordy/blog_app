0c8cef3ba74f7818e12dbfa300058597b4e32866
Zil Norvilis <zilasino27@gmail.com>
2021-04-02 16:32:09 +0200

    Adds Article update and destroy methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-      @articles = Article.all
+    @articles = Article.all
   end
 
   def show
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Artcile.new(article_params)
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -21,8 +21,29 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private 
-  def article_params
-    params.require(:article).permit(:title,:body)
+  def edit
+    @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
